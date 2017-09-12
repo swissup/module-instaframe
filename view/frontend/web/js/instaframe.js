c4c42743413a5774259e853ccfc7ebf7e741c6f8
roman chupryna <rommmmka@gmail.com>
2017-09-12 17:27:50 +0300

    Added flipping for description css/js

--- view/frontend/web/js/instaframe.js
+++ view/frontend/web/js/instaframe.js
@@ -14,6 +14,24 @@ define([
             this._super();
             require(['waterfall'], function(waterfall) {
                 waterfall(el);
+                window.addEventListener('resize', function () {
+                    waterfall(el);
+                });
+            });
+
+            /* FlipOnClick */
+            $(function() {                       //run when the DOM is ready
+                $(".instaframe-item").click(function() {  //use a class, since your ID gets mangled
+                    $(this).toggleClass("clicked");      //add the class to the clicked element
+                });
+
+                $(".instaframe-likes").click(function(event){
+                    event.stopPropagation();
+                });
+
+                $(".instaframe-comments").click(function(event){
+                    event.stopPropagation();
+                });
             });
         }
     });
